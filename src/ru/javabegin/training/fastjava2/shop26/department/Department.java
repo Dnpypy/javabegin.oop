42283bb88ee7ff50fc63d7fcacdadf84b3f15146
Dnpypy <denlatypoff@yandex.ru>
2022-03-17 17:25:05 +0300

    create interfaces Department, Employee

---
+++ src/ru/javabegin/training/fastjava2/shop26/department/Department.java
@@ -0,0 +1,8 @@
+package ru.javabegin.training.fastjava2.shop26.department;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public interface Department {
+}
---
+++ src/ru/javabegin/training/fastjava2/shop26/service/Employee.java
@@ -0,0 +1,8 @@
+package ru.javabegin.training.fastjava2.shop26.service;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public interface Employee {
+}
